5988518adfef3978a780c474e8b1d7a3a46b3f07
Peter Murphy <26548438+pete-murphy@users.noreply.github.com>
2024-05-11 23:15:20 -0400

    Add minimal ref example

--- spago.dhall
+++ spago.dhall
@@ -13,6 +13,7 @@ to generate this file without the comments in this block.
 { name = "react-starter"
 , dependencies =
   [ "console"
+  , "debug"
   , "effect"
   , "exceptions"
   , "foldable-traversable"
--- src/App.purs
+++ src/App.purs
@@ -3,33 +3,35 @@ module App where
 import Prelude
 
 import Data.Foldable as Foldable
+import Data.Nullable as Nullable
 import Effect.Class.Console as Console
-import Effect.Timer as Timer
 import React.Basic.DOM as DOM
-import React.Basic.DOM.Events as DOM.Events
-import React.Basic.Events as Events
-import React.Basic.Hooks (Component, (/\))
+import React.Basic.Hooks (Component)
 import React.Basic.Hooks as Hooks
+import Web.DOM.Element as Element
+import Web.Event.Event (EventType(..))
+import Web.Event.EventTarget as EventTarget
 
 mkApp :: Component Unit
 mkApp = do
   Hooks.component "App" \_ -> Hooks.do
-    text /\ setText <- Hooks.useState' ""
-    timer /\ setTimer <- Hooks.useState 0
-    Hooks.useEffect text do
-      handle <- Timer.setInterval 1_000 (setTimer (_ + 1))
-      pure (setTimer (\_ -> 0) *> Timer.clearInterval handle)
-    pure
-      ( DOM.div_
-          [ DOM.input
-              { onChange:
-                  Events.handler DOM.Events.targetValue
-                    ( Foldable.traverse_ \value -> do
-                        Console.log value
-                        setText value
-                    )
-              , value: text
-              }
-          , DOM.p_ [ DOM.text (show timer) ]
-          ]
-      )
+    ref <- Hooks.useRef Nullable.null
+
+    Hooks.useEffect unit do
+      maybeNode <- Hooks.readRefMaybe ref
+      let
+        maybeElement = maybeNode >>= Element.fromNode
+
+      listener <- EventTarget.eventListener \_ -> do
+        Console.log "click"
+
+      Foldable.for_ maybeElement \element -> do
+        EventTarget.addEventListener (EventType "click") listener true (Element.toEventTarget element)
+
+      pure
+        ( Foldable.for_ maybeElement \element -> do
+            EventTarget.removeEventListener (EventType "click") listener true (Element.toEventTarget element)
+        )
+
+    pure (DOM.div { ref, children: [ DOM.text "Click me!" ] })
+
